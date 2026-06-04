6ae3cf1ea24981f069e91d132ad46db4770300a4
EdsonAguiar888 <edsonaguiar888@gmail.com>
2026-06-04 09:11:08 -0300

    Correção e atualizacao

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,8 @@ import imagemPlanejar from '../assets/imgPlanejar.png';
 import imagemCalculadora from '../assets/imgCalculadora.png';
 import imagemCarrinho from '../assets/imgCarrinho.png';
 
+import imagemgps from '../assets/imagemgps.png';
+
 function getBateriaClasse(pct) {
   if (pct > 50) return 'bb-battery-fill';
   if (pct > 20) return 'bb-battery-fill medium';
@@ -80,7 +82,10 @@ export default function Home({ usuario }) {
           <div className="station-title">Rota otimizada para recarga</div>
 
           <Link to="/otimizador" className="station-card optimizer-preview-card" aria-label="Abrir otimizador de rotas">
-            <div className="map-wrapper">
+            
+            <img src={imagemgps} style={{ height: '170px',  width: '250px' }} alt="Ícone Estações" className="card-icon" />
+            
+            {/* <div className="map-wrapper">
               <div className="optimizer-mini-map" aria-hidden="true">
                 <span className="mini-map-road mini-map-road-main" />
                 <span className="mini-map-road mini-map-road-cross" />
@@ -91,7 +96,7 @@ export default function Home({ usuario }) {
                 <span className="mini-marker mini-station" />
                 <span className="mini-marker mini-destination" />
               </div>
-            </div>
+            </div> */}
 
             <div className="details-wrapper">
               <div className="station-name">Prévia do otimizador de rota</div>
